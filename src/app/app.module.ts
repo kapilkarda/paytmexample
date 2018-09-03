f7482a9b15842b49c488e419c62b74fea26a5849
kapilkardaitop <kapil.karda@itoptix.com>
2018-09-03 17:18:20 +0530

    NEw Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { HttpModule } from '@angular/http';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import { Http, Headers, RequestOptions,RequestMethod, Response, URLSearchParams } from '@angular/http';
+
+
+declare var PaytmPlugin: any;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +11,27 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private http: Http, public platform: Platform) {
+    
 
   }
+
+  callbutton(){
+      this.platform.ready().then(() => {
+        let orderId = "karda12345";
+        let email = "kkarda77@gmail.com";
+        let mobile = "9981101934";
+        let callbackURL = "https://securegw.paytm.in/theia/paytmCallback?ORDER_ID="+orderId;
+        this.http.get("https://www.mahakaalstore.com/Paytm_Web_Sample_Kit_PHP-master/PaytmKit/generateChecksum.php?MID=Engine57819657649623&ORDER_ID="+orderId+"&CUST_ID="+orderId+"&INDUSTRY_TYPE_ID=Retail109&CHANNEL_ID=WEB&TXN_AMOUNT=1&WEBSITE=Engineerwap&EMAIL="+email+"&MOBILE_NO="+mobile,{}).subscribe((response: Response) => {
+            let res = response.json();
+            console.log(res);
+            PaytmPlugin.payWithPaytm(orderId, orderId, email, mobile, '1', callbackURL, res.CHECKSUMHASH, 'production', function (success) { 
+              console.log(success); 
+            }, function (failure) { 
+              alert(failure); 
+            });
+        });
+      });
+    }
 
 }
